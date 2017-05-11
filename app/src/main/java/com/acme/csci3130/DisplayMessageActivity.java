b5d9953eab404b36a2d9298446c4cd7b4aed38ae
asmadal <as510329@dal.ca>
2017-05-10 23:53:10 -0300

    adding a responsive button and text field to the UI

--- app/src/main/java/com/acme/csci3130/DisplayMessageActivity.java
+++ app/src/main/java/com/acme/csci3130/DisplayMessageActivity.java
@@ -11,6 +11,7 @@ public class DisplayMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
         
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
